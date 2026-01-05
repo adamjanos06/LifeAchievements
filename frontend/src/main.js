d5c4c0a316f421a35e06aafe6540898f07317ceb
SzAndrasPeter <sz.andras.peter@gmail.com>
2026-01-05 17:03:35 +0100

    user stays signed in2

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -10,17 +10,15 @@ import '@assets/main.css'
 
 import axios from 'axios'
 
-// 🔹 BACKEND API URL (FONTOS: legyen benne /api is!)
 axios.defaults.baseURL = "http://backend.vm1.test/api"
 
-// 🔹 TOKEN BETÖLTÉSE LOCALSTORAGE-BÓL
 const token = localStorage.getItem("token")
 
 if (token) {
   axios.defaults.headers.common["Authorization"] = `Bearer ${token}`
-  console.log("Auth header set from stored token ✔")
+  console.log("Auth header set from stored token")
 } else {
-  console.log("No token found in storage ❌")
+  console.log("No token found in storage")
 }
 
 createApp(App)
